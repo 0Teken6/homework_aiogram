3ad791f064e489d244c6031f0c9d87bbb56704b6
0Teken6 <timu.rev006@gmail.com>
2024-07-09 16:07:06 +0600

    dop_dz

--- handlers/echo.py
+++ handlers/echo.py
@@ -11,6 +11,22 @@ async def echo(message: types.Message):
     elif message.text == 'game':
         emoji = random.choice(['⚽', '🎰', '🏀', '🎯', '🎳', '🎲'])
         await bot.send_dice(chat_id=message.from_user.id, emoji=emoji)
+    elif message.text == 'game_2':
+        await bot.send_message(chat_id=message.from_user.id, text='First, your turn!')
+        msg_user = await bot.send_dice(chat_id=message.from_user.id)
+        await sleep(4)
+        await bot.send_message(chat_id=message.from_user.id, text=f'Your score: {msg_user.dice.value}')
+        await sleep(1)
+        await bot.send_message(chat_id=message.from_user.id, text='My turn!')
+        msg_bot = await bot.send_dice(chat_id=message.from_user.id)
+        await sleep(4)
+        await bot.send_message(chat_id=message.from_user.id, text=f'My score: {msg_bot.dice.value}')
+        if msg_user.dice.value > msg_bot.dice.value:
+            await message.answer(text=f'{msg_user.dice.value} > {msg_bot.dice.value}\nYou won!')
+        elif msg_user.dice.value < msg_bot.dice.value:
+            await message.answer(text=f'{msg_user.dice.value} < {msg_bot.dice.value}\nI won!')
+        else:
+            await message.answer(text=f'{msg_user.dice.value} = {msg_bot.dice.value}\nIt\'s a draw!')
     else:
         await message.answer(message.text)
 
